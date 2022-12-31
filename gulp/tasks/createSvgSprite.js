fee6f914213344caa1306d27ed3685a75b7335d7
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2022-12-31 07:34:35 +0530

    fonts and images optimization

--- gulp/tasks/createSvgSprite.js
+++
@@ -1,7 +0,0 @@
-import { stacksvg } from 'gulp-stacksvg';
-
-export const createSvgSprite = () => {
-  return gulp.src('source/img/icons/*.svg')
-    .pipe(stacksvg({ output: 'sprite' }))
-    .pipe(gulp.dest('build/img'));
-}
--- gulp/tasks/createWebp.js
+++
@@ -1,9 +0,0 @@
-import squoosh from 'gulp-libsquoosh';
-
-export const createWebp = () => {
-  return gulp.src('source/img/**.*.{jpg,png}')
-    .pipe(squoosh({
-      webp: {}
-    }))
-    .pipe(gulp.dest('build/img'));
-}
